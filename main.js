4ef210a4adafbfcc07bdef9a24f3595ce0a5836c
Stanislav Stolbov <stolbovstanislav@mail.ru>
2023-12-05 19:06:23 +0500

    Added rules generation.

--- main.js
+++ main.js
@@ -12,6 +12,20 @@ const secret = randomBytes(256).toString('hex');
 
 const computerMove = args[Math.floor(Math.random() * args.length)];
 
+const userLoseMoves = [];
+const userDrawMove = computerMove;
+const half = (args.length - 1) / 2;
+const computerMoveIndex = args.indexOf(computerMove);
+for (let shift = 1; shift <= half; shift++) {
+  userLoseMoves.push(args.at(computerMoveIndex - shift));
+}
+const userWinMoves = args.filter((arg) => (arg !== userDrawMove) && (!userLoseMoves.includes(arg)));
+console.log('userWinMoves', userWinMoves);
+console.log('userLoseMoves', userLoseMoves);
+console.log('userDrawMove', userDrawMove);
+console.log('half', half);
+console.log('computerMoveIndex', computerMoveIndex);
+
 const hmac = createHmac('sha512', secret).digest('hex');
 
 console.log('args', args);
